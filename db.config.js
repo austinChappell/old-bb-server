1df4c6ddd5fef5133ddbf1165b059807051d97ee
austinChappell <Austin.W.Chappell@gmail.com>
2018-03-30 23:16:09 -0500

    update dbconfig var

--- db.config.js
+++ db.config.js
@@ -1,6 +1,8 @@
+const dbConfig = process.env.NODE_ENV === 'development' ? {} : {
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+}
+
 module.exports = {
-  dbConfig: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true,
-  }
+  dbConfig
 }
